defbc8f542cecebfd67e480db06e9150ff46b84b
Dennis Chen <dechen@redhat.com>
2025-08-05 13:41:45 -0400

    Add check that disabling resets stats to 0
    Signed-off-by: Dennis Chen <dechen@redhat.com>

--- tools/testing/selftests/drivers/net/netdevsim/ethtool-stats.sh
+++ tools/testing/selftests/drivers/net/netdevsim/ethtool-stats.sh
@@ -9,19 +9,23 @@ set -o pipefail
 
 NSIM_NETDEV=$(make_netdev)
 
-# enable mock stats
 echo y > $NSIM_DEV_DFS/ethtool/mock_stats/enabled
 
-# mock stats were just enabled, make sure they aren't too high
 stat=$(ethtool -S $NSIM_NETDEV | grep "hw_out_of_buffer" | awk '{print $2}')
-((stat < 10))
-check_code $? "ethtool stats show >= 10 packets after first enablement"
+((stat == 0))
+check_code $? "ethtool stats show > 0 packets immediately after enabling"
 
 sleep 2.5
 
 stat=$(ethtool -S $NSIM_NETDEV | grep "hw_out_of_buffer" | awk '{print $2}')
 ((stat >= 20))
 check_code $? "ethtool stats show < 20 packets after 2.5s passed"
+
+echo n > $NSIM_DEV_DFS/ethtool/mock_stats/enabled
+
+stat=$(ethtool -S $NSIM_NETDEV | grep "hw_out_of_buffer" | awk '{print $2}')
+((stat == 0))
+check_code $? "ethtool stats show > 0 packets after disabling"
 
 if [ $num_errors -eq 0 ]; then
     echo "PASSED all $((num_passes)) checks"
